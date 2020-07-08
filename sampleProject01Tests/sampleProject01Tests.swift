4f972b1a2d8fadce8f9d827e9cfc76b8213b4314
y.suzuki <yu.suzuki.forjob@gmail.com>
2020-07-08 23:16:14 +0900

    new create

---
+++ sampleProject01Tests/sampleProject01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  sampleProject01Tests.swift
+//  sampleProject01Tests
+//
+//  Created by 鈴木勇介 on 2020/07/08.
+//  Copyright © 2020 鈴木勇介. All rights reserved.
+//
+
+import XCTest
+@testable import sampleProject01
+
+class sampleProject01Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
