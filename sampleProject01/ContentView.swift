2ea196362a14f9345e4250a321f0a449b98d5ea8
y.suzuki <yu.suzuki.forjob@gmail.com>
2020-07-08 23:13:14 +0900

    new create

---
+++ sampleProject01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sampleProject01
+//
+//  Created by 鈴木勇介 on 2020/07/08.
+//  Copyright © 2020 鈴木勇介. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
